94b47afc2afb2bc1b05cafac583e6c1293787a9f
EvgeniyKurlyanchik <91161381+EvgeniyKurlyanchik@users.noreply.github.com>
2022-02-17 23:16:42 +0300

    Initial commit

---
+++ src/main/java/example/Cat.java
@@ -0,0 +1,19 @@
+package example;
+
+@Table(title = "cat")
+public class Cat {
+    @Column
+    String name;
+
+    @Column
+    String color;
+
+    @Column
+    int age;
+
+    public Cat(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+}
